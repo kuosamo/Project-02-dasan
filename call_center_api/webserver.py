5c2d99510524259d13cde95e37c573b70ce9e7ad
김경수 <KKS@gimgyeongsuui-MacBook-Pro.local>
2016-12-22 11:12:16 +0900

    modify answering

--- call_center_api/webserver.py
+++ call_center_api/webserver.py
@@ -10,26 +10,31 @@ from machine_learning import Machine
 from answering import Answer
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def hello():
+    return 'Dasan call center Auto System!'
 
 @app.route('/test')
 def hello_json():
     data = {'name' : 'Kyeoung Soo', 'family' : 'Kim'}
     return jsonify(data)
 
+@app.route('/example')
+def example():
+    ex = { '/question/search/<keyword>': u'DB에 있는 다산콜센터 내역에서 keyword부분을 검색하여 보여준다.',
+           '/question/answer/<keyword>': u'keyword에 질문을 입력하면 분류된 디렉토리와 함께 답변을 보여준다.' }
+    return jsonify(ex)
+
 # dasandao에서 select_in_question함수 호출
 @app.route('/question/search/<keyword>')
-def search_news(keyword):
+def search_db(keyword):
     dasandao = DasanDAO()
     data = dasandao.select_in_question(str(keyword))
     return jsonify(data)
 
 # machine_learning 에서 model를 만든후, answering에서 answer_bot함수 호출
 @app.route('/question/answer/<keyword>')
-def answer(keyword):
+def answer_db(keyword):
     # model
     machine = Machine()
     trained = machine.training()
